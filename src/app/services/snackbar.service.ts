e5836e63f8899e29d173aa375a86460e0dca6565
Uttam Galoriya <19ce037@gardividyapith.ac.in>
2023-08-21 15:22:37 +0530

    snackbar time change

--- src/app/services/snackbar.service.ts
+++ src/app/services/snackbar.service.ts
@@ -9,7 +9,7 @@ export class SnackbarService {
   constructor(private snackBar: MatSnackBar) { }
   showNotification(displayMessage: string, buttonText: string, messageType: 'error' | 'success' | 'info') {
     this.snackBar.open(displayMessage, buttonText, {
-      duration: 200000, horizontalPosition: 'center', verticalPosition: 'top', panelClass: messageType
+      duration: 2000, horizontalPosition: 'center', verticalPosition: 'top', panelClass: messageType
     })
   }
 }
